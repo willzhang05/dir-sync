8e46f15b75c7d5da782cfabe5e831ecdbef8349e
William Zhang <willzhang05@gmail.com>
2018-09-03 12:28:55 -0400

    read_in

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,42 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 use dns_lookup::{lookup_host, lookup_addr};
 
-#[derive(Serialize, Deserialize, Debug)]
+use serde_json::{Value, Error};
+
 
 struct Directory {
     uri: String
 }
 
+fn read_in() -> Result<(), Error> {
+    let mut config_file = File::open("config.json").unwrap();
+    let mut data = String::new();
+    config_file.read_to_string(&mut data).unwrap();
+    
+    let mut v: Value = serde_json::from_str(&data)?;
+    println!("{} {}", v["servers"]["name"], v["servers"]["address"]);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    /*for argument in env::args_os() {
+        println!("{:?}", argument);    
+    }*/
+
+    let result = read_in();
+    let result = match result {
+         Ok(()) => (),
+        Err(error) => {
+            panic!("There was a problem reading in config.json: {:?}", error)
+        },
+    };
+    
 }
