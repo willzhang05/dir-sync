138de6f6e29beca859ca9f3e1e4f1347a083289f
William Zhang <willzhang05@gmail.com>
2018-09-10 14:43:43 -0400

    figure out some things

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate serde_json;
 
 //use std::env;
 use std::fs::File;
+use std::fs::DirEntry;
 use std::io::Read;
 
 use dns_lookup::{lookup_host, lookup_addr};
@@ -18,30 +19,28 @@ struct Directory {
     uri: String
 }
 
-fn read_in() -> Result<(), Error> {
+fn read_in() -> Result<(Vec<Value>,Vec<Value>), Error> {
     let mut config_file = File::open("config.json").unwrap();
     let mut data = String::new();
     config_file.read_to_string(&mut data).unwrap();
     
-    let mut v: Value = serde_json::from_str(&data)?;
+    let v: Value = serde_json::from_str(&data)?;
 
     //println!("{:?} {:?}", v["servers"][0]["name"], v["servers"][0]["address"]);
     
-    //let mut servers = Vec::new();
-    //let mut servers: Vec<Value> = ;
+    let servers: Vec<Value> = v["servers"].as_array().unwrap().to_vec();
     
-    /*for s in  {
-        /*
-        let mut server = json!({
-            "name": s["name"],
-            "hostname": s["hostname"]
-        });*/
+    for s in servers {
         println!("{:?} {:?}", s["name"], s["hostname"]);
-        //servers.push(server);
-    }*/
+    }
  
+    let local_dirs: Vec<Value> = v["local_dir"].as_array().unwrap().to_vec();
 
-    Ok(())
+    for dir in local_dirs {
+        println!("{:?} {:?}", dir["label"], dir["uri"]);
+    }
+
+    Ok((servers, local_dirs))
 }
 
 fn main() {
@@ -49,10 +48,9 @@ fn main() {
     /*for argument in env::args_os() {
         println!("{:?}", argument);    
     }*/
-
     let result = read_in();
     let result = match result {
-        Ok(()) => (),
+        Ok((servers, local_dirs)) => (),
         Err(error) => {
             panic!("There was a problem reading in config.json: {:?}", error)
         },
